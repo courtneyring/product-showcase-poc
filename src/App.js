6bdec697f6ed0834d2616991f25ecd74b8479619
Courtney Ring <courtneyring3@gmail.com>
2024-04-02 16:14:53 -0400

    word placement, model rotation test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import { Canvas } from '@react-three/fiber';
+import Experience from './Experience';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [4, - 2, 6]
+      }}
+    >
+      <Experience />
+      <color attach="background" args={['black']} />
+    </Canvas>
+
   );
 }
 
---
+++ src/Experience.js
@@ -0,0 +1,63 @@
+import { CameraControls } from '@react-three/drei';
+import { Scroll, ScrollControls, useScroll } from '@react-three/drei';
+import { useFrame } from '@react-three/fiber';
+import { useState } from 'react';
+
+
+const Model = () => {
+  const scroll = useScroll();
+  const [rotation, setRotation] = useState(0);
+
+  useFrame(() => {
+    setRotation((Math.PI * 2) * scroll.range(0, 1))
+  })
+
+  return (
+    <mesh rotation={[0, rotation, 0]}>
+      <torusGeometry />
+      <meshStandardMaterial color={'green'} />
+    </mesh>
+  )
+
+}
+
+
+const Experience = () => {
+
+  const pages = 3;
+
+  return (
+    <>
+      <ambientLight intensity={3} />
+      {/* <CameraControls /> */}
+
+      <ScrollControls pages={pages}>
+        <Model />
+        <Scroll>
+
+        </Scroll>
+        <Scroll html>
+          <div className='container'>
+            {
+              [...Array(pages)].map((page, idx) => {
+                return <div key={idx} className='section'>
+                  <div className='words'>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                  </div>
+                </div>
+              })
+            }
+
+          </div>
+
+
+        </Scroll>
+
+      </ScrollControls>
+
+
+    </>
+  )
+}
+
+export default Experience;
