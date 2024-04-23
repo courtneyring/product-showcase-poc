b5aa5e8b9c3994a3874e22b6f59bf75febcf0b88
Courtney Ring <courtneyring3@gmail.com>
2024-04-23 15:19:51 -0400

    add sequencing, save state

--- src/Experience.js
+++ src/Experience.js
@@ -1,5 +1,6 @@
 import { Scroll, ScrollControls, useScroll, Environment, useGLTF, AccumulativeShadows, RandomizedLight, OrbitControls } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
+import pageData from './assets/data';
 
 import {
   editable as e,
@@ -36,13 +37,13 @@ const Model = () => {
 
 const Experience = () => {
 
-  const pages = 3;
+  const pages = 6;
 
   return (
     <>
       <e.ambientLight intensity={0.5} theatreKey='ambientLight' />
       <spotLight position={[0, 15, 0]} angle={0.3} penumbra={1} castShadow intensity={2} shadow-bias={-0.0001} />
-      <OrbitControls />
+      {/* <OrbitControls /> */}
 
       <PerspectiveCamera
         theatreKey="Camera"
@@ -53,7 +54,7 @@ const Experience = () => {
         far={70}
       />
 
-      <ScrollControls pages={pages}>
+      <ScrollControls pages={pageData.length}>
 
         <Environment background={false} blur={100} preset='sunset' theatreKey='environment' intensity={0.5} />
         <Model />
@@ -67,12 +68,17 @@ const Experience = () => {
         </Scroll>
         <Scroll html>
           <div className='container'>
+            <div className='section'>
+              <h1>Introducing All-New Range Rover</h1>
+            </div>
             {
-              [...Array(pages)].map((page, idx) => {
+              pageData.map((page, idx) => {
                 return <div key={idx} className='section'>
-                  <div className='words'>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  <div className='content'>
+                    <h2 >{page.title}</h2>
+                    <p>{page.body}</p>
                   </div>
+
                 </div>
               })
             }
---
+++ src/assets/data.js
@@ -0,0 +1,39 @@
+const pageData = [
+  {
+    "title": "A Refined Exterior",
+    "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+
+  },
+  {
+    "title": "LED Headlights with signature DRL",
+    "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+
+  },
+  {
+    "title": "3D Surround Camera",
+    "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+
+  },
+  {
+    "title": "Heated, elecric, power fold door mirrors",
+    "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+
+  },
+  {
+    "title": "Heated rear windows with privacy glass",
+    "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+
+  },
+  {
+    "title": "Dynamic Stability Control",
+    "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+
+  },
+  {
+    "title": "Rear Wash Wiper",
+    "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+
+  },
+]
+
+export default pageData;
